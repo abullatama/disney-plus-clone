ea8ec870f837a692c7b514b604ca179feee236fd
abullatama <abullatama@gmail.com>
2021-05-05 12:53:23 +0700

    add react slick carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 const Home = (props) => {
   return (
-    <MyTitle>
-      <h2>Home Page</h2>
-    </MyTitle>
+    <Container>
+      <ImgSlider />
+    </Container>
   );
 };
 
-const MyTitle = styled.section`
+const Container = styled.main`
   position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
   top: 72px;
-  h2 {
-    color: white;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
   }
 `;
 
