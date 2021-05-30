21f33f95b93083b90d785fabf067436e59ba8e9e
abullatama <abullatama@gmail.com>
2021-05-30 13:27:43 +0700

    Changing the Default React Browser Tab Title and Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { auth, provider } from "../firebase";
 import {
   selectUserName,
@@ -83,7 +83,9 @@ const Header = (props) => {
       ) : null}
 
       <Logo>
-        <img src="disney-hotstar-logo-dark.svg" alt="Disney+" />
+        <Link to="/">
+          <img src="disney-hotstar-logo-dark.svg" alt="Disney+" />
+        </Link>
       </Logo>
 
       {!userName ? (
